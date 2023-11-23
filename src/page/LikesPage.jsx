ca668b9664b67bb2e1af6ce7563796b4b04ccad7
Pauline Remi <pauline.remi@lapiscine.pro>
2023-11-23 11:54:09 +0100

    add useState message

--- src/page/LikesPage.jsx
+++ src/page/LikesPage.jsx
@@ -3,14 +3,28 @@ import { useState } from "react";
 function LikesPage() {
 
     const [likes, setLikes] = useState(0);
+    const [displayMessage, setDisplayMessage] = useState(false)
 
     const clickLikes = () => {
-        setLikes(likes + 1);
+        if (likes < 5 ) {
+            setLikes(likes + 1);
+        } else {
+            setDisplayMessage(true)
+        }    
     }
 
+    const closeMessage = () => {
+        setDisplayMessage(false);
+        setLikes(0)
+    }
     
     return(
         <main>
+            {displayMessage && 
+            <div>
+                <p>Vous ne pouvez pas liker plus de 5 fois</p>
+                <button onClick={closeMessage}>Fermer le message</button>
+            </div>}
             <button onClick={clickLikes}>Likes</button>
             <p>Vous avez likés {likes} fois</p>
         </main>
